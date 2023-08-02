912e9feac9f13c7dfcc46101d3709bb3e9357579
AdrianIkeaba <adrianikeaba@gmail.com>
2023-08-03 00:54:10 +0100

    Added a nav drawer to the activity.

--- app/src/main/java/com/ghostdev/quickjot/MainActivity.kt
+++ app/src/main/java/com/ghostdev/quickjot/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.ghostdev.quickjot
 
 import android.graphics.Color
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
-import android.media.Image
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.RelativeSizeSpan
 import android.view.Menu
-import android.widget.ImageView
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
@@ -18,10 +21,15 @@ import com.ghostdev.quickjot.databinding.ActivityMainBinding
 import com.ghostdev.quickjot.repository.NotesRepository
 import com.ghostdev.quickjot.viewmodel.NoteViewModel
 import com.ghostdev.quickjot.viewmodel.NoteViewModelFactory
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var noteViewModel: NoteViewModel
     private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,12 +42,24 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        var navController = navHostFragment.navController
+        val navController = navHostFragment.navController
+        drawerLayout = binding.drawerLayout
 
+        // Set up the ActionBarDrawerToggle
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            binding.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
         // Set up the AppBarConfiguration with top-level destinations to show the home/up button
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment))
-        // Setup the toolbar with the NavController and AppBarConfiguration
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // Set up the toolbar with the NavController and AppBarConfiguration
         NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration)
 
         // Add the OnDestinationChangedListener
@@ -47,6 +67,8 @@ class MainActivity : AppCompatActivity() {
             when (destination.id) {
                 R.id.homeFragment -> {
                     supportActionBar?.title = "All Jots"
+                    val firstMenuItem: MenuItem = binding.navigationView.menu.getItem(0)
+                    firstMenuItem.isChecked = true
                 }
                 R.id.updateNoteFragment -> {
                     supportActionBar?.title = "Update Jot"
@@ -59,17 +81,41 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        val roundedDrawable = ContextCompat.getDrawable(this, R.drawable.nav_drawer_background)
+        ViewCompat.setBackground(binding.navigationView, roundedDrawable)
 
+       binding.navigationView.inflateHeaderView(R.layout.nav_drawer_header)
+
+        val navView: NavigationView = binding.navigationView
+        val menu: Menu = navView.menu
+        for (i in 0 until menu.size()) {
+                val menuItem: MenuItem = menu.getItem(i)
+                val spanString = SpannableString(menuItem.title.toString())
+                spanString.setSpan(RelativeSizeSpan(1.25f), 0, spanString.length, 0)
+                menuItem.title = spanString
+        }
     }
 
     private fun setUpViewModel() {
         val notesRepository = NotesRepository(NoteDatabase(this))
         val viewModelProviderFactory = NoteViewModelFactory(application, notesRepository)
-        noteViewModel = ViewModelProvider(this, viewModelProviderFactory) [NoteViewModel::class.java]
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NoteViewModel::class.java)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.home_menu, menu)
         return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
